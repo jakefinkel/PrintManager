694da0571bd3613e55b93c3a58301f45a3591955
vagrant <vagrant@php-training>
2017-04-19 03:35:54 -0700

    welcome user

--- index.php
+++ index.php
@@ -4,12 +4,14 @@ namespace PrintManager;
 ini_set('display_errors', 1);
 
 spl_autoload_register(function ($class) {
-    $requirePath = str_replace('\\', '/', $class) . ".php";
+    $requirePath = str_replace('\\', '/', $class) . '.php';
     require_once($requirePath);
 });
 
-$rgb  = new Printer\RgbPrinter($name = "Microsoft XPS Document Writer");
-$cmyk = new Printer\CmykPrinter($name = "XEROX Phaser 8100");
+echo 'Welcome, ' . $_POST['email'] . '!';
+
+$rgb  = new Printer\RgbPrinter($name = 'Microsoft XPS Document Writer');
+$cmyk = new Printer\CmykPrinter($name = 'XEROX Phaser 8100');
 
 $rgb->print();
 $cmyk->print();
